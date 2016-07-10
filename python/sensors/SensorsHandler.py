564308b263fc79a3229555f4b9b07e1ba83bf3c8
Matthias Gabriel <matthias.gabriel@etit.tu-chemnitz.de>
2016-07-10 12:32:04 +0200

    Added testcase

--- python/sensors/SensorsHandler.py
+++ python/sensors/SensorsHandler.py
@@ -2,14 +2,19 @@
 
 class SensorsHandler(object):
     def __init__(self, enabled_sensors):
-        self._enabled_sensors = enabled_sensors
+        self._sensor_list = list()
         for sensor in self._enabled_sensors:
-            self.import_sensor_module(sensor)
+            self.create_sensors(sensor)
         
-    def import_sensor_module(self, sensor):
+    def create_sensors(self, sensor):
         if (sensor.name() == 'HTU21D'):
             import drivers
             import drivers.HTU21D
             import drivers.HTU21D.Sensor
             print('imported')
             sensor = drivers.HTU21D.Sensor.Sensor(sensor.options())
+            self._sensor_list.append(sensor)
+            
+    def measure():
+        for sensor in self._sensor_list:
+            print(sensor.measure())
--- python/test.py
+++ python/test.py
@@ -3,3 +3,4 @@ from  sensors import SensorsHandler as sensors_handler
 
 cfg_reader = cfg.ConfigReader('../config/station.cfg')
 handler = sensors_handler.SensorsHandler(cfg_reader.parse_sensor_list())
+handler.measure()
